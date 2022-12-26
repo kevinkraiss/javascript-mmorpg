4f9db89ebf612a704690d819eb7eebb762f1ee94
Kevin Kraiss <kevinkraiss@gmail.com>
2022-12-26 16:10:01 -0600

    sets up indivdual user as a moving box

--- index.cjs
+++ index.cjs
@@ -8,14 +8,41 @@ const PORT = 4200
 const { Server } = require("socket.io")
 const io = new Server(server)
 
+class ConnectedUser {
+  constructor(socket) {
+    this.id_ = _USERS.length
+    this.pos_ = [(Math.random() * 20), 0, Math.random() * 20]
+    this.socket_ = socket
+    this.socket_.on('pos', (d) => {
+      this.pos_ = [...d]
+      this.SpamEveryone_()
+    })
+    this.SpamEveryone_()
+  }
+  SpamEveryone_() {
+    this.socket_.emit('pos', [this.id_, this.pos_])
+
+    for (let i= 0; i < _USERS.length; ++i) {
+      _USERS[i].socket_.emit('pos', [this.id_, this.pos_])
+      this.socket_.emit(_USERS[i].id_, _USERS[i].pos_)
+      // console.log(_USERS)
+    }
+  }
+
+}
+
+const _USERS = []
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+    _USERS.push(new ConnectedUser(socket))
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
--- public/js/scene.js
+++ public/js/scene.js
@@ -1,8 +1,185 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 
-module.exports = renderScene
+
+class BasicWorldDemo {
+  constructor() {
+    this._Initialize();
+  }
+
+  _Initialize() {
+    this._threejs = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this._threejs.shadowMap.enabled = true;
+    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+    this._threejs.setPixelRatio(window.devicePixelRatio);
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(this._threejs.domElement);
+
+    window.addEventListener('resize', () => {
+      this._OnWindowResize();
+    }, false);
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 1000.0;
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this._camera.position.set(75, 20, 0);
+
+    this._scene = new THREE.Scene();
+
+    let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+    light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
+    this._scene.add(light);
+
+    light = new THREE.AmbientLight(0x101010);
+    this._scene.add(light);
+
+    const controls = new OrbitControls(
+      this._camera, this._threejs.domElement);
+    controls.target.set(0, 20, 0);
+    controls.update();
+
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.setPath( '../public/resources/' )
+    .load([
+        'posx.bmp',
+        'negx.bmp',
+        'posy.bmp',
+        'negy.bmp',
+        'posz.bmp',
+        'negz.bmp'
+    ]);
+    this._scene.background = texture;
+
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(100, 100, 10, 10),
+        new THREE.MeshStandardMaterial({
+            color: 0xFFFFFF,
+          }));
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    this._scene.add(plane);
+
+    // const box = new THREE.Mesh(
+    //   new THREE.BoxGeometry(2, 2, 2),
+    //   new THREE.MeshStandardMaterial({
+    //       color: 0xFFFFFF,
+    //   }));
+    // box.position.set(0, 1, 0);
+    // box.castShadow = true;
+    // box.receiveShadow = true;
+    // this._scene.add(box);
+
+    // for (let x = -8; x < 8; x++) {
+    //   for (let y = -8; y < 8; y++) {
+    //     const box = new THREE.Mesh(
+    //       new THREE.BoxGeometry(2, 2, 2),
+    //       new THREE.MeshStandardMaterial({
+    //           color: 0x808080,
+    //       }));
+    //     box.position.set(Math.random() + x * 5, Math.random() * 4.0 + 2.0, Math.random() + y * 5);
+    //     box.castShadow = true;
+    //     box.receiveShadow = true;
+    //     this._scene.add(box);
+    //   }
+    // }
+
+    // const box = new THREE.Mesh(
+    //   new THREE.SphereGeometry(2, 32, 32),
+    //   new THREE.MeshStandardMaterial({
+    //       color: 0xFFFFFF,
+    //       wireframe: true,
+    //       wireframeLinewidth: 4,
+    //   }));
+    // box.position.set(0, 0, 0);
+    // box.castShadow = true;
+    // box.receiveShadow = true;
+    // this._scene.add(box);
+
+
+    document.addEventListener('keyup', (e) =>
+    {
+        switch(e.keyCode) {
+            case 87: //w
+                this.mainPlayer.position.z += 2.5
+                break
+            case 65: //a
+                this.mainPlayer.position.x -= 2.5
+                break
+            case 83: //s
+                this.mainPlayer.position.z -= 2.5
+                break
+            case 68: //d
+                this.mainPlayer.position.x += 2.5
+                break
+        }
+        this.socket_.emit('pos', this.mainPlayer.position.toArray())
+    }, false)
+
+    this.players_ = {}
+    this.mainPlayer = null
+
+    this.socket_ = io('localhost:4200', {transports: ['websocket']})
+    this.socket_.on('pos', (d) => {
+        const [id, pos] = d
+
+        if (!(id in this.players_)) {
+        const box = new THREE.Mesh(
+            new THREE.BoxGeometry(2, 2, 2),
+            new THREE.MeshStandardMaterial({
+                color: 0xFFFFFF,
+            }));
+          box.position.set(...d);
+          box.castShadow = true;
+          box.receiveShadow = true;
+          this._scene.add(box);
+        this.players_[id] = box
+        if (!this.mainPlayer) {
+            this.mainPlayer = box
+            }
+        }
+        this.players_[id].position.set(...pos)
+    })
+
+    this._RAF();
+  }
+
+  _OnWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  _RAF() {
+    requestAnimationFrame(() => {
+      this._threejs.render(this._scene, this._camera);
+      this._RAF();
+    });
+  }
+}
+
+
+let _APP = null;
+
+window.addEventListener('DOMContentLoaded', () => {
+  _APP = new BasicWorldDemo();
+});
